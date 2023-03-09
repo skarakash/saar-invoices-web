eb566f63a3fc7d71eb6546ade1682b4137888c38
skarakash <karakash.sergey@gmail.com>
2023-03-09 11:01:51 +0200

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,30 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from 'react'
+import { apiUrl } from './config/config'
+
+const getData = async () => {
+  try {
+    const res = await fetch(`${apiUrl}/get`);
+    return await res.json()
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+
+  useEffect(  () => {
+    getData().then(res => {
+      setData(res)
+    })
+  }, [])
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+        <>
+          {data.length > 0 ?
+            JSON.stringify(data, null, 2): null
+          }
+        </>
   )
 }
 
---
+++ src/config/config.ts
@@ -0,0 +1,1 @@
+export const apiUrl = "https://invoices-api.up.railway.app/invoices"
